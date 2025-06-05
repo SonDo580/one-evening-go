64e5056bda48319460d6d631df74f57e1dccdaae
Son Do <dominhson580@gmail.com>
2025-06-05 09:10:41 +0700

    project - ls

---
+++ project-ls/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	files := listFiles("testdata")
+	for _, file := range files {
+		fmt.Println(file)
+	}
+}
+
+func listFiles(dirname string) []string {
+	var dirs []string
+
+	files, err := os.ReadDir(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		dirs = append(dirs, f.Name())
+	}
+
+	return dirs
+}
